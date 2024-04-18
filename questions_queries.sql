2a5d94c5def49d372bda755891f916e9621399ff
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-04-17 23:27:48 -0400

    Update questions_queries.sql

--- questions_queries.sql
+++ questions_queries.sql
@@ -1,4 +1,10 @@
+---How many orders per month? Months with most orders
 SELECT
-  *
+  EXTRACT(YEAR_MONTH FROM order_date) AS order_month,
+  COUNT(order_id) order_count
 FROM
   luisalva.north_wind_traders.orders
+GROUP BY
+  order_month
+ORDER BY
+  order_month DESC;
