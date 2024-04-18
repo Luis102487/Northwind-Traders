f2091aedc13eca2fdad21ce14a8482970b4f2dcf
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-04-17 23:33:47 -0400

    Update questions_queries.sql

--- questions_queries.sql
+++ questions_queries.sql
@@ -1,4 +1,4 @@
----How many orders per month? Months with most orders
+-- How many orders per month? Months with most orders
 SELECT
   EXTRACT(YEAR_MONTH FROM order_date) AS order_month,
   COUNT(order_id) order_count
@@ -8,3 +8,27 @@ GROUP BY
   order_month
 ORDER BY
   order_month DESC;
+
+-- What orders haven't shipped yet?
+SELECT
+  *
+FROM
+  luisalva.north_wind_traders.orders
+WHERE
+  shipped_date IS NULL
+
+
+
+Company with most orders? top 5
+Employee with most orders processed?
+Shipper with most orders processed?
+Shipper tyat make most money
+What is the city where most order go? What is the country?
+Top 5 selling products? Most selling products?
+Company that spend the most?
+Yearly revenue
+Monthly revenue
+Top 5 expensiest order?
+Order with quantity ordered?
+Most selling category
+Least selling category
