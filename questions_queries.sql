b2ca05fcc891ff451b04d1398cf4345048b9da9b
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-04-18 21:55:23 -0400

    Update questions_queries.sql

--- questions_queries.sql
+++ questions_queries.sql
@@ -9,6 +9,7 @@ GROUP BY
 ORDER BY
   order_month DESC;
 
+
 -- What orders haven't shipped yet?
 SELECT
   *
@@ -17,6 +18,7 @@ FROM
 WHERE
   shipped_date IS NULL
 
+  
 -- Total orders by Company. Top 5 companies with the most orders
 SELECT
   c.company_name,
@@ -32,6 +34,7 @@ GROUP BY
 ORDER BY
   order_count DESC  
 
+  
 -- Total cost per order. Top 5 expesive orders.
 SELECT
   c.company_name,
@@ -52,6 +55,27 @@ GROUP BY
   c.company_name
 ORDER BY
   order_total_cost DESC
+
+  
+-- Total quantity ordered by company(all orders combined). Top 5 companies with most quanitity ordered.
+SELECT
+  c.company_name,
+  sum (od.quantity) AS total_quantity_ordered
+FROM
+  luisalva.north_wind_traders.customers c
+JOIN
+  luisalva.north_wind_traders.orders o
+ON
+  o.customer_id = c.customer_id
+JOIN
+  luisalva.north_wind_traders.order_details od
+ON
+  od.order_id = o.order_id
+GROUP BY
+  c.company_name
+ORDER BY
+  total_quantity_ordered DESC
+
 
   
 Employee with most orders processed?
