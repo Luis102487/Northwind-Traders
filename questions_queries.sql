f243be1be34e11779ac4e6a54bf4ec01b376f85e
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-04-18 20:00:34 -0400

    Update questions_queries.sql

--- questions_queries.sql
+++ questions_queries.sql
@@ -22,15 +22,15 @@ SELECT
   c.company_name,
   COUNT(o.order_id) AS order_count
 FROM
-  luisalva.north_wind_traders.orders o
-JOIN
   luisalva.north_wind_traders.customers c
+JOIN
+  luisalva.north_wind_traders.orders o
 ON
-  o.customer_id = c.customer_id
+  c.customer_id = o.customer_id
 GROUP BY
   c.company_name
 ORDER BY
-  order_count DESC 
+  order_count DESC  
 
 
 Employee with most orders processed?
